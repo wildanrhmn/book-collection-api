52c8e3c641ba3b7042013bca607d19890239ebb5
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2023-11-08 20:50:04 +0700

    [author] complete author CRUD

--- src/modules/author/author.service.ts
+++ src/modules/author/author.service.ts
@@ -15,3 +15,52 @@ export const listAuthors = async(): Promise<Author[]> => {
         }
     });
 }
+
+export const getAuthor = async(id: string): Promise<Author | null> => {
+    return db.author.findUnique({
+        where: {
+            id
+        }
+    });
+}
+
+export const createAuthor = async(author: Omit<Author, "id">): Promise<Author> => {
+    const { firstName, lastName } = author;
+    return db.author.create({
+        data: {
+            firstName,
+            lastName
+        },
+        select: {
+            id: true,
+            firstName: true,
+            lastName: true
+        }
+    });
+}
+
+export const updateAuthor = async(id: string, author: Omit<Author, "id">): Promise<Author | null> => {
+    const { firstName, lastName } = author;
+    return db.author.update({
+        where: {
+            id
+        },
+        data: {
+            firstName,
+            lastName
+        },
+        select: {
+            id: true,
+            firstName: true,
+            lastName: true
+        }
+    });
+}
+
+export const deleteAuthor = async(id: string): Promise<Author | null> => {
+    return db.author.delete({
+        where: {
+            id
+        },
+    });
+}
